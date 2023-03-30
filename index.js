64874b18aa8cf0968ad6cee577c110a439139844
Shamsul009 <arifeen1707008@gmail.com>
2023-03-30 07:11:15 +0600

    Server Create

--- index.js
+++ index.js
@@ -9,18 +9,34 @@
 // const fs = require('fs');
 // fs.writeFileSync('myFile.txt','Hello');
 
-const EventEmitter = require('events');
-//const { emit } = require('process');
-const emitt = new EventEmitter();
+// const EventEmitter = require('events');
+// //const { emit } = require('process');
+// const emitt = new EventEmitter();
 
-emitt.on('bell',({period,madam})=>{
-    console.log(`Its time to run because ${period} end and ${madam} start walking`);
-})
+// emitt.on('bell',({period,madam})=>{
+//     console.log(`Its time to run because ${period} end and ${madam} start walking`);
+// })
 
-emitt.emit('bell',{
-    period : '2nd',
-    madam : 'Mrs.Y'
+// emitt.emit('bell',{
+//     period : '2nd',
+//     madam : 'Mrs.Y'
+// });
+
+//Server Create
+const http = require('node:http');
+const server = http.createServer((req,res)=>{
+    if(req.url ==='/home'){
+        res.write('Welcome');
+        res.end();
+    }
+    else if(req.url ==='/aboutus'){
+        res.write('welcome to about page');
+        res.end();
+    }
 });
+
+server.listen(4000);
+console.log('connection on');
 
 
 
