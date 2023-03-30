a80f738cd788d55e652d2c7543fe7aafe8f55f6a
Shamsul009 <arifeen1707008@gmail.com>
2023-03-31 02:18:12 +0600

    Server Create Using postman

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
-//Global Object & Module System
-// const path = require('node:path');
+const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder')
+const app ={};
 
-// const myPath = 'H:\\NodeJs\\index.js';
-// console.log(path.basename(myPath));
+app.config = {
+    port : 3000,
+};
 
+app.createServer = ()=>{
+    const serverCreate = http.createServer(app.handleReqRes);
+    serverCreate.listen(app.config.port,()=>{
+        console.log('Server is Running');
+    })
+};
 
+app.handleReqRes = (req,res)=>{
+    const parseUrl = url.parse(req.url,true);
+    const pathname = parseUrl.pathname;
+    const trimPath = pathname.replace(/^\/+|\/+$/g,'');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parseUrl.query;
+    const headerObject = req.headers;
 
-// const fs = require('fs');
-// fs.writeFileSync('myFile.txt','Hello');
+    const decoder = new StringDecoder('utf-8');
+    let fullRes = '';
 
-// const EventEmitter = require('events');
-// //const { emit } = require('process');
-// const emitt = new EventEmitter();
+    req.on('data',(buffer)=>{
+        fullRes+=decoder.write(buffer);
+    });
 
-// emitt.on('bell',({period,madam})=>{
-//     console.log(`Its time to run because ${period} end and ${madam} start walking`);
-// })
+    req.on('end',()=>{
+        fullRes += decoder.end();
+        console.log(fullRes);
+        res.end('Data is finished');
+    })
+    
+};
 
-// emitt.emit('bell',{
-//     period : '2nd',
-//     madam : 'Mrs.Y'
-// });
-
-//Server Create
-const http = require('node:http');
-const server = http.createServer((req,res)=>{
-    if(req.url ==='/home'){
-        res.write('Welcome');
-        res.end();
-    }
-    else if(req.url ==='/aboutus'){
-        res.write('welcome to about page');
-        res.end();
-    }
-});
-
-server.listen(4000);
-console.log('connection on');
-
-
-
-
-
-
-
-
-
+app.createServer();
